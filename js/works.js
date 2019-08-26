c958bf5d90975855e6a921cc99bbc3d42b613e23
Esteban Ramon <esteban.ramon@verndale.com>
2019-08-25 22:05:58 -0500

    Esteban Perez website

---
+++ js/works.js
@@ -0,0 +1,459 @@
+const imagesInf = {
+    coll1: [
+        {
+            file: '../images/Snapshots.jpg',
+            name: 'Snapshots',
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '140 x 135 cm'
+            
+        }, 
+        {
+             file: '../images/guap.jpg',
+            name: `08/09/2018 22:01:33 <br>
+            0° 12' 1,98" S, 78° 28' 32,69" W <br>
+            2661,05m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/hos.jpg',
+            name: `26/10/2018 13:50:36 <br> 
+            0° 10' 21,09" S, 78° 29' 21,33" W <br>
+            2797,74m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/pared.jpg',
+            name: `23/11/2018 11:02:42 <br>
+            2° 53' 50,90" S, 79° 0' 16,62" W <br>
+            2555,05m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/sunset.jpg',
+            name: `24/11/2018 18:24:04 <br>
+            2° 53' 42,51" S, 79° 0' 39,03" W <br>
+            2144,74m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/aereo.jpg',
+            name: `26/11/2018 11:02:51 <br>   
+            2° 53' 23,40" S, 78° 59' 15,20" W <br>
+            2532,42m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/elec.jpg',
+            name: `07/12/2018 9:56:45 <br>
+            0° 11' 46,43" S, 78° 25' 55,14" W <br>
+            2372,75m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+             file: '../images/Norte.jpg',
+            name: `05/02/2019 10:33:05 <br>
+            0° 6' 57,68" S, 78° 28' 42,37" W <br>
+            2829,21m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+        {
+            file: '../images/vereda.jpg',
+            name:`08/02/2019 7:34:56 <br>
+            0° 12' 23,81" S, 78° 25' 29,90" W <br>
+            2347,32m`,
+            year: '2019',
+            description: 'Oil on paper',
+            dimensions: '21 x 28 cm '
+        },
+    ],
+    coll2: [
+        {
+            file: '../images/obra_1.jpg',
+            name: 'Moving Foward',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '30 x 40 cm'
+        },
+        {
+            file: '../images/obra_2.jpg',
+            name: 'Neohabitat',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '29 x 21 cm'
+        },
+        {
+            file: '../images/obra_3.jpg',
+            name: 'Tropa en Movimiento',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '19 x 17,5 cm'
+        }
+    ],
+    coll3: [
+        {
+            file: '../images/T7.jpg',
+            name: 'Omi',
+            year: '2018',
+            description: 'Acrylic on canvas',
+            dimensions: '240 x 190 cm'
+        }, 
+        {
+            file: '../images/T6-2.jpg',
+            name: 'Ahogado en la realidad',
+            year: '2018',
+            description: 'Acrylic on canvas',
+            dimensions: '240 x 190 cm'
+        },
+        {
+            file: '../images/T5.jpg',
+            name: 'Release',
+            year: '2017',
+            description: 'Acrylic on canvas',
+            dimensions: '100 x 54 cm'
+        },
+        {
+            file: '../images/T1.jpg',
+            name: 'Heiße Ohren',
+            year: '2017',
+            description: 'Acrylic on canvas',
+            dimensions: '89 x 135 cm'
+        },
+        {
+            file: '../images/T4.jpg',
+            name: 'Buoyancy',
+            year: '2017',
+            description: 'Acrylic on canvas',
+            dimensions: '100 x 94 cm'
+        },
+        {
+            file: '../images/T3.jpg',
+            name: 'Burnout',
+            year: '2017',
+            description: 'Acrylic on canvas',
+            dimensions: '89 x 135 cm'
+        },
+        {
+            file: '../images/T8.jpg',
+            name: 'Voxel',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '140 x 180 cm'
+        },
+        {
+            file: '../images/T9.jpg',
+            name: 'Entanglement',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '140 x 180 cm'
+        },
+        {
+            file: '../images/T10.jpg',
+            name: 'Simulation',
+            year: '2019',
+            description: 'Acrylic on canvas',
+            dimensions: '140 x 180 cm'
+        } 
+    ],
+    coll4: [
+        {
+            file: '../images/R1.jpg',
+            name: 'Animal Laborans',
+            year: '2018',
+            description: 'Acrylic & oil pastel on paper',
+            dimensions: '53 x 38 cm'
+        }, 
+        {
+            file: '../images/R3.jpg',
+            name: 'Parallel Profile',
+            year: '2018',
+            description: 'Acrylic & oil pastel on paper',
+            dimensions: '50 x 70 cm'
+        },
+        {
+            file: '../images/R4.jpg',
+            name: 'Self-controlled',
+            year: '2018',
+            description: 'Acrylic on paper',
+            dimensions: '50 x 70 cm'
+        },
+         {
+            file: '../images/R5.jpg',
+            name: 'Exhaustion',
+            year: '2018',
+            description: 'Acrylic on paper',
+            dimensions: '50 x 70 cm'
+        },
+         {
+            file: '../images/R6.jpg',
+            name: 'Hiperpassive',
+            year: '2018',
+            description: 'Acrylic & ink on paper',
+            dimensions: '52 x 75 cm'
+        },
+         {
+            file: '../images/R7.jpg',
+            name: 'Nude pic',
+            year: '2019',
+            description: 'Acrylic & oil pastel on paper',
+            dimensions: '25 x 35 cm'
+        },
+         {
+            file: '../images/R8.jpg',
+            name: 'Expecting',
+            year: '2019',
+            description: 'Acrylic on paper',
+            dimensions: '50 x 70 cm'
+        },
+         {
+            file: '../images/R9.jpg',
+            name: 'Pseudo-vice',
+            year: '2019',
+            description: 'Acrylic on paper',
+            dimensions: '50 x 70 cm'
+        },
+        {
+            file: '../images/Triplete-13.jpg',
+            name: 'Triplete',
+            year: '2018',
+            description: '',
+            Gallery: 'No lugar',
+        },
+    ],
+    coll5: [
+        {
+            file: '../images/Triplete-2.jpg',
+            name: 'Found Objects',
+            year: '2018',
+            description: '',
+            gallery: 'No lugar'
+        }, 
+        {
+            file: '../images/X5_1.jpg',
+            name: 'Reliquary',
+            year: '2017',
+            description: 'Assemblage',
+            dimensions: '40 x 61 x 23 cm'
+        },
+        {
+            file: '../images/X5_8.jpg',
+            name: 'Reliquary',
+            year: '2017',
+            description: 'Assemblage',
+            dimensions: '40 x 61 x 23 cm'
+        },
+         {
+            file: '../images/X5_5.jpg',
+            name: 'Reliquary',
+            year: '2017',
+            description: 'Assemblage',
+            dimensions: '40 x 61 x 23 cm'
+        },
+         {
+            file: '../images/Triplete-12.jpg',
+            name: 'Democracy is my imaginary friend',
+            year: '2018',
+            description: 'Mixed media on wood',
+            dimensions: '120 x 173 cm'
+        },
+         {
+            file: '../images/Triplete-14.jpg',
+            name: 'Found Objects',
+            year: '2018',
+            Gallery: 'No lugar',
+            description: '',
+  
+        },
+         {
+            file: '../images/Triplete-15.jpg',
+           name: 'Found Objects',
+           year: '2018',
+           description: '',
+           Gallery: 'No lugar'
+        },
+        {
+            file: '../images/Triplete-16.jpg',
+            name: 'El Chebas',
+            year: '2017',
+            description: 'Mixed media on wood',
+            dimensions: '114 x 89 cm'
+        },
+        {
+            file: '../images/Triplete-17.jpg',
+            name: 'Untitled',
+            year: '2017',
+            description: 'Mixed media on wood',
+            dimensions: '119 x 93 cm'
+        },
+         {
+            file: '../images/Triplete-20.jpg',
+           name: 'Found Objects',
+           year: '2018',
+           Gallery: 'No lugar',
+           description: ''
+        },
+         {
+            file: '../images/Triplete-21.jpg',
+            name: 'El Cara Delavigne',
+            year: '2017',
+            description: 'Mixed media on pallets',
+            dimensions: '60 x 135 x 47 cm'
+        },
+        {
+            file: '../images/Triplete-23.jpg',
+            name: 'Alptraum',
+            year: '2017',
+            description: 'Mixed media on wood',
+            dimensions: '60 x 54 cm'
+        },
+        {
+            file: '../images/Triplete-24.jpg',
+            name: 'Der Erfinder',
+            year: '2017',
+            description: 'Mixed media on wood',
+            dimensions: '60 x 60 cm'
+        },
+        {
+            file: '../images/Triplete-25.jpg',
+            name: 'Ausländer',
+            year: '2018',
+            description: 'Mixed media on wood',
+            dimensions: '63 x 80 cm'
+        },
+        {
+            file: '../images/Triplete-29.jpg',
+            name: 'Found Objects',
+            year: '2018',
+            Gallery: 'No lugar',
+            description: '',
+        },
+        {
+            file: '../images/Triplete-28.jpg',
+            name: 'Tercermundista',
+            year: '2017',
+            description: 'Assemblage',
+            dimensions: '142 x 90 x 9 cm'
+        },
+        {
+            file: '../images/X10.jpg',
+            name: "Guess who's back?",
+            year: '2018',
+            description: 'Mixed media on wood',
+            dimensions: '89 x 173 x 14 cm'
+        },
+    ],
+    coll6: [
+        {
+           file: '../images/P-7.jpg',
+            name: 'Time, space & matter',
+            year: '2016',
+            description: 'Acrylic on canvas',
+            dimensions: '90 x 100 cm'
+        }, 
+        {
+           file: '../images/P-10.jpg',
+            name: 'Let go',
+            year: '2016',
+            description: 'Acrylic on canvas',
+            dimensions: '100 x 130 cm'
+        },
+        {
+            file: '../images/P-12.jpg',
+            name: 'La deidad',
+            year: '2016',
+            description: 'Acrylic on canvas',
+            dimensions: '180 x 200 cm'
+        }
+        
+    ],
+  };
+  
+  
+let slideIndex = 0;
+
+const urlParams = new URLSearchParams(window.location.search);
+const collectionName = urlParams.get('col');
+const collection = imagesInf[collectionName];
+const collectionSize = collection.length;
+const displayedImage = document.getElementById("displayed-image"); 
+const displayedImageBig = document.getElementById("displayed-image-big"); 
+const imageViewer = document.getElementById("image-viewer");
+const imageViewerInfo  = document.getElementById("viewer-image-info");
+const keyRightCode = 39;
+const keyLeftCode = 37;
+const keyExitCode = 27;
+
+
+document.body.onkeydown = function(event){
+    const eventKeyCode = event.keyCode;
+    const isViewerHidden = imageViewer.classList.contains("hidden-section");
+    if(eventKeyCode === keyRightCode) {
+        changeImage(1);
+    }
+    if(eventKeyCode === keyLeftCode) {
+        changeImage(-1);
+    }
+    if(eventKeyCode === keyExitCode && !isViewerHidden) {
+        toggleImageViewer();
+    }
+}
+
+onInit();
+
+function onInit() {
+  slideIndex = 0;
+  setImageData(slideIndex);
+}
+
+function changeImage(value) {
+  
+  slideIndex = slideIndex + value; 
+  if(slideIndex < 0) {
+    slideIndex = collectionSize - 1;
+  }
+
+  if (slideIndex === collectionSize) {
+    slideIndex = 0;
+  }
+  setImageData(slideIndex);
+}
+
+function setImageData(slideIndex) {
+  const imageData = collection[slideIndex];
+  const newSrc = imageData.file + "?random="+ Math.random();  
+  displayedImage.src= newSrc;
+  displayedImageBig.src= newSrc;
+  const imageGallery = imageData.gallery || "";
+  $(".work-title").html(imageData.name);
+  $(".work-year").html(imageData.year);
+  $(".work-description").html(imageData.description);
+  $(".work-size").html(imageData.dimensions); 
+  $(".work-gallery").html(imageGallery); 
+
+}
+
+function toggleImageViewer () {
+  imageViewer.classList.toggle("hidden-section");
+  displayedImage.classList.toggle("hidden-section")
+}
+
+function toggleImageViewerImageInfo () {
+  imageViewerInfo.classList.add("visible-image-info");
+  setTimeout(function() {
+    imageViewerInfo.classList.remove("visible-image-info");
+  }, 5000)
+}
